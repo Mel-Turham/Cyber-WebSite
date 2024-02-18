81a698aaaf68c3f27176f1069caaa733fd64482c
Mel/ <tchoikuemel06@gmail.com>
2024-02-18 03:01:51 +0100

    create the about product first time

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -3,8 +3,10 @@ import Headers from '../../Components/Headers/Headers';
 import './ProductDetails.scss';
 import Image from '../../assets/images/Image.png';
 import ScreenSize from '../../assets/Svgs/Screen size.svg';
-import Delivrey from '../../assets/Svgs/Delivery.svg'
+import Delivrey from '../../assets/Svgs/Delivery.svg';
+import {details} from '../../../src/datas/details';
 
+console.log(details);
 const ProductDetails = () => {
 	const Memories = ['128GB', '256GB', '512GB', '1TB'];
 	const [changeText, setChangeText] = useState(true);
@@ -15,6 +17,8 @@ const ProductDetails = () => {
 	return (
 		<>
 			<Headers />
+
+			{/* Produts details */}
 			<section className='Details'>
 				<article className='Details__left'>
 					<img src={Image} alt='' loading='lazy' />
@@ -112,7 +116,7 @@ const ProductDetails = () => {
 					<div className='deliveries'>
 						<div className='delivery'>
 							<span>
-								<img src={Delivrey } alt='Delivrery icon' loading='lazy' />
+								<img src={Delivrey} alt='Delivrery icon' loading='lazy' />
 							</span>
 
 							<div className='delivery-item'>
@@ -122,7 +126,7 @@ const ProductDetails = () => {
 						</div>
 						<div className='delivery'>
 							<span>
-								<img src={Delivrey } alt='Delivrery icon' loading='lazy' />
+								<img src={Delivrey} alt='Delivrery icon' loading='lazy' />
 							</span>
 
 							<div className='delivery-item'>
@@ -132,7 +136,7 @@ const ProductDetails = () => {
 						</div>
 						<div className='delivery'>
 							<span>
-								<img src={Delivrey } alt='Delivrery icon' loading='lazy' />
+								<img src={Delivrey} alt='Delivrery icon' loading='lazy' />
 							</span>
 
 							<div className='delivery-item'>
@@ -144,7 +148,37 @@ const ProductDetails = () => {
 				</div>
 			</section>
 
-			
+			{/* Details about the produts */}
+
+			<section className='produtsAbouts'>
+				<div className='topdetail'>
+					<h2>Details</h2>
+					<p>
+						Just as a book is judged by its cover, the first thing you notice
+						when you pick up a modern smartphone is the display. Nothing
+						surprising, because advanced technologies allow you to practically
+						level the display frames and cutouts for the front camera and
+						speaker, leaving no room for bold design solutions. And how good
+						that in such realities Apple everything is fine with displays. Both
+						critics and mass consumers always praise the quality of the picture
+						provided by the products of the Californian brand. And last year s
+						6.7-inch Retina panels, which had ProMotion, caused real admiration
+						for many.
+					</p>
+				</div>
+
+				<div className='product-details-main'>
+					<h3>screen</h3>
+					<div className='main-items'>
+						{details.map((detail) => (
+							<div className='items' key={detail.name}>
+								<span className='d-name'>{detail.name}</span>
+								<span className='d-value'>{detail.values}</span>
+							</div>
+						))}
+					</div>
+				</div>
+			</section>
 		</>
 	);
 };
---
+++ src/datas/details.js
@@ -0,0 +1,29 @@
+const details = [
+	{
+		name: 'Screen digonal',
+		values: ' 6.7',
+	},
+	{
+		name: 'The screen reaolution',
+		values: '2796x1290',
+	},
+
+	{
+		name: 'The screen refresh rate',
+		values: '120 Hz',
+	},
+	{
+		name: 'The pixel density',
+		values: '460pi',
+	},
+	{
+		name: 'Screen type',
+		values: 'OLED',
+	},
+	{
+		name: 'Additionally',
+		values: 'Dynamic Island',
+	},
+];
+
+export { details };
